7c83d9dd72a11ca079982f4c9aa597a490233bdb
Andrija Cajic <ancajic@gmail.com>
2015-01-06 17:09:00 +0100

    minify

--- Mafia/app/mafia.js
+++ Mafia/app/mafia.js
@@ -2557,17 +2557,19 @@ app.controller('RegisterController', function ($scope, $location, $timeout, user
         $location.path('');
     };
 
-    var register = function() {
+
+
+    var registerUser = function() {
         var newUser = $scope.newUser;
         if (newUser.username.length == 0) {
-            $scope.infos.push({type: 'danger', msg: 'Username must not be empty'});
+            $scope.registerInfos.push({type: 'danger', msg: 'Username must not be empty'});
         }
         if (newUser.password.length == 0) {
-            $scope.infos.push({type: 'danger', msg: 'Password must not be empty'});
+            $scope.registerInfos.push({type: 'danger', msg: 'Password must not be empty'});
             return;
         }
         if (newUser.repeat_password != newUser.password) {
-            $scope.infos.push({type: 'danger', msg: 'Repeated password don\'t match the original one.'});
+            $scope.registerInfos.push({type: 'danger', msg: 'Repeated password don\'t match the original one.'});
             return;
         }
 
@@ -2575,20 +2577,20 @@ app.controller('RegisterController', function ($scope, $location, $timeout, user
         $scope.isLoading = true;
         createUserPromise.then(function(createdUser) {
             $timeout(function() {
-                $scope.infos.push({type : 'success', msg: 'Successfully created user ' + createdUser.username + '. Check your email in order to confirm your email address.'});
+                $scope.registerInfos.push({type : 'success', msg: 'Successfully created user ' + createdUser.username + '. Check your email in order to confirm your email address.'});
             });
             $scope.isLoading = false;
         }, function(reason) {
             $scope.isLoading = false;
             for (var key in reason.httpObj.responseJSON) {
                 if (reason.httpObj.responseJSON.hasOwnProperty(key))
-                    $scope.infos.push({type : 'danger', msg: key + " " + reason.httpObj.responseJSON[key] });
+                    $scope.registerInfos.push({type : 'danger', msg: key + " " + reason.httpObj.responseJSON[key] });
             }
         });
     };
 
-    $scope.closeInfoAlert = function(index) {
-        $scope.infos.splice(index, 1);
+    $scope.closeRegisterInfoAlert = function(index) {
+        $scope.registerInfos.splice(index, 1);
     };
 
     init();
@@ -2596,8 +2598,8 @@ app.controller('RegisterController', function ($scope, $location, $timeout, user
     function init() {
         $scope.newUser = newUser;
         $scope.back = back;
-        $scope.register = register;
-        $scope.infos = [];
+        $scope.registerUser = registerUser;
+        $scope.registerInfos = [];
     }
 
 }); 
